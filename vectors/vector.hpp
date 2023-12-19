52336f002074484a4f666d5d4860cab9ed3e9050
Sagar <04017711721_iot@vvips.edu>
2023-12-19 19:58:39 +0530

    Add .vscode/settings.json and implement push_back function in vector.hpp

--- vectors/vector.hpp
+++ vectors/vector.hpp
@@ -1,7 +1,23 @@
 #ifndef VECTOR_H 
 #define VECTOR_H 
 
+template <typename T > 
 class vector {
+
+    private:
+        int size;
+        int capacity;
+        T* items;
+
+    public:
+    vector();
+    vector(int n ,const T  &value );
+    vector(const vector& v1 );
+
+// implementation of the push opeartion 
+    void push_back(T &object ){
+
+    }
 
 };
 
